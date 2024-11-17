973b2808daf6785485b24b2181e2bc60fca71153
Amin <asaa47721@gmail.com>
2024-11-17 22:36:35 +0330

    Add tabs for shop details and products, load products on scrolling

--- lib/shop_details.dart
+++ lib/shop_details.dart
@@ -14,13 +14,40 @@ class ShopDetailsPage extends StatefulWidget {
   _ShopDetailsPageState createState() => _ShopDetailsPageState();
 }
 
-class _ShopDetailsPageState extends State<ShopDetailsPage> {
+class _ShopDetailsPageState extends State<ShopDetailsPage>
+    with SingleTickerProviderStateMixin {
   Map<String, dynamic>? shopDetails;
+  late TabController _tabController;
+  late ScrollController _scrollController;
+  List<dynamic> products = [];
+  bool isLoadingProducts = false;
+  bool allProductsLoaded = false;
+  int productPage = 1;
 
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _scrollController = ScrollController();
+    _scrollController.addListener(_onScroll);
     _fetchShopDetails();
+    _fetchProducts();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels >=
+            _scrollController.position.maxScrollExtent - 100 &&
+        !isLoadingProducts &&
+        !allProductsLoaded) {
+      _fetchProducts();
+    }
   }
 
   Future<void> _fetchShopDetails() async {
@@ -45,185 +72,184 @@ class _ShopDetailsPageState extends State<ShopDetailsPage> {
     }
   }
 
+  Future<void> _fetchProducts() async {
+    if (isLoadingProducts || allProductsLoaded) return;
+
+    setState(() {
+      isLoadingProducts = true;
+    });
+
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('access_token');
+
+    final url = Uri.parse(
+        'http://localhost:8101/v1/shop/${widget.shopId}/products?page=$productPage&size=20');
+
+    final response = await http.get(url, headers: {
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode == 200) {
+      List<dynamic> newProducts =
+          json.decode(utf8.decode(response.bodyBytes))['items'];
+
+      setState(() {
+        if (newProducts.isEmpty) {
+          allProductsLoaded = true;
+        } else {
+          products.addAll(newProducts);
+          productPage++;
+        }
+        isLoadingProducts = false;
+      });
+    } else {
+      setState(() {
+        isLoadingProducts = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('بارگیری محصولات با شکست مواجه شد.')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("جزئیات فروشگاه")),
+      appBar: AppBar(
+        title: const Text("جزئیات فروشگاه"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: "جزئیات"),
+            Tab(text: "محصولات"),
+          ],
+        ),
+      ),
       body: shopDetails == null
           ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView(
-                children: [
-                  // Placeholder for shop image
-                  Container(
-                    height: 200,
-                    color: Colors.grey[300],
-                    child: const Center(child: Text('تصویر فروشگاه')),
-                  ),
-
-                  const SizedBox(height: 16),
-
-                  // Shop name
-                  Text(
-                    shopDetails!['name'],
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-
-                  const SizedBox(height: 24),
-
-                  // Shop address
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.location_on, color: Colors.green),
-                          const SizedBox(width: 4),
-                          Text(
-                            "آدرس: ${shopDetails!['address'] ?? 'نامشخص'}",
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  // Rating
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.star, color: Colors.orange),
-                          const SizedBox(width: 4),
-                          Text(
-                            "امتیاز: ${shopDetails!['rating'] ?? 'نامشخص'}",
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  // Mobile number
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.phone_android, color: Colors.blue),
-                          const SizedBox(width: 4),
-                          Text(
-                            "شماره تماس: ${shopDetails!['mobile'] ?? 'نامشخص'}",
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  // Is Physical
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.storefront, color: Colors.purple),
-                          const SizedBox(width: 4),
-                          Text(
-                            "فروشگاه حضوری: ${shopDetails!['is_physical'] ? 'بله' : 'خیر'}",
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  // Category
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.category, color: Colors.teal),
-                          const SizedBox(width: 4),
-                          Text(
-                            "دسته‌بندی: ${shopDetails!['category'] ?? 'نامشخص'}",
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  // Joined date
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.calendar_today,
-                              color: Colors.deepOrange),
-                          const SizedBox(width: 4),
-                          Text(
-                            "تاریخ پیوستن: ${_formatDate(shopDetails!['created_at'])}",
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 10), // Add more spacing
-
-                  // Description Section
-                  const Divider(thickness: 1), // Divider for clarity
-                  const SizedBox(height: 16),
-                  const Text(
-                    "توضیحات",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    shopDetails!['description'] ?? 'نامشخص',
-                    style: const TextStyle(fontSize: 16, height: 1.8),
-                    textAlign: TextAlign.justify,
-                  ),
-                  const SizedBox(height: 30), // Add more spacing at the bottom
-                ],
-              ),
+          : TabBarView(
+              controller: _tabController,
+              children: [
+                // Tab 1: Shop Details
+                _buildShopDetailsTab(),
+                // Tab 2: Shop Products
+                _buildProductsTab(),
+              ],
             ),
     );
   }
 
-  // Function to format the DateTime as year/month/day
+  Widget _buildShopDetailsTab() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          // Placeholder for shop image
+          Container(
+            height: 200,
+            color: Colors.grey[300],
+            child: const Center(child: Text('تصویر فروشگاه')),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            shopDetails!['name'],
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 24),
+
+          _buildDetailRow(
+              Icons.location_on, Colors.green, "آدرس", shopDetails!['address']),
+
+          const SizedBox(height: 10),
+
+          _buildDetailRow(
+              Icons.star, Colors.orange, "امتیاز", shopDetails!['rating']),
+
+          const SizedBox(height: 10),
+
+          _buildDetailRow(Icons.phone_android, Colors.blue, "شماره تماس",
+              shopDetails!['mobile']),
+
+          const SizedBox(height: 10),
+
+          _buildDetailRow(Icons.storefront, Colors.purple, "فروشگاه حضوری",
+              shopDetails!['is_physical'] ? 'بله' : 'خیر'),
+
+          const SizedBox(height: 10),
+
+          _buildDetailRow(Icons.category, Colors.teal, "دسته‌بندی",
+              shopDetails!['category']),
+
+          const SizedBox(height: 10),
+
+          _buildDetailRow(Icons.calendar_today, Colors.deepOrange,
+              "تاریخ پیوستن", _formatDate(shopDetails!['created_at'])),
+
+          const SizedBox(height: 10),
+          const Divider(thickness: 1),
+          const SizedBox(height: 16),
+          const Text(
+            "توضیحات",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline),
+          ),
+          const SizedBox(height: 8),
+          Text(shopDetails!['description'] ?? 'نامشخص',
+              style: const TextStyle(fontSize: 16, height: 1.8),
+              textAlign: TextAlign.justify),
+          const SizedBox(height: 30),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailRow(
+      IconData icon, Color color, String label, dynamic value) {
+    return Row(
+      children: [
+        Icon(icon, color: color),
+        const SizedBox(width: 4),
+        Text("$label: ${value ?? 'نامشخص'}",
+            style: const TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+
+  Widget _buildProductsTab() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: products.length + (allProductsLoaded ? 0 : 1),
+        itemBuilder: (context, index) {
+          if (index == products.length) {
+            return isLoadingProducts
+                ? const Center(child: CircularProgressIndicator())
+                : const SizedBox.shrink();
+          }
+
+          var product = products[index];
+          return ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text(product['name'] ?? 'نامشخص'),
+            subtitle: Text("قیمت: ${product['price'] ?? 'نامشخص'}"),
+          );
+        },
+      ),
+    );
+  }
+
   String _formatDate(String? date) {
     if (date == null) return 'نامشخص';
-
     try {
       final DateTime parsedDate = DateTime.parse(date);
-
       Jalali j = Jalali.fromDateTime(parsedDate);
-
       return '${j.day}-${j.month}-${j.year}';
     } catch (e) {
       return 'نامشخص';
