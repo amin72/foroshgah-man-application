1461e9d115684d223b8b19959d825c09ac031fa6
Amin <asaa47721@gmail.com>
2024-11-20 06:13:16 +0330

    Implement user profile update

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -14,6 +14,13 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   Map<String, dynamic>? userData;
   bool isLoading = true;
+  bool isEditing = false;
+
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _provinceController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
 
   @override
   void initState() {
@@ -35,8 +42,10 @@ class _ProfilePageState extends State<ProfilePage> {
       final response = await http.get(url, headers: headers);
 
       if (response.statusCode == 200) {
+        final data = json.decode(utf8.decode(response.bodyBytes));
         setState(() {
-          userData = json.decode(utf8.decode(response.bodyBytes));
+          userData = data;
+          _populateControllers();
           isLoading = false;
         });
       } else {
@@ -47,67 +56,200 @@ class _ProfilePageState extends State<ProfilePage> {
     }
   }
 
+  void _populateControllers() {
+    _firstNameController.text = userData?['first_name'] ?? '';
+    _lastNameController.text = userData?['last_name'] ?? '';
+    _provinceController.text = userData?['province'] ?? '';
+    _cityController.text = userData?['city'] ?? '';
+    _addressController.text = userData?['address'] ?? '';
+  }
+
+  Future<void> _updateUserInfo() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('access_token') ?? '';
+
+      final url = Uri.parse('http://localhost:8100/v1/user/info/');
+      final headers = {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      };
+
+      // Prepare the fields for updating, keeping unchanged fields the same as before
+      final Map<String, dynamic> updatedFields = {};
+
+      updatedFields['first_name'] = _firstNameController.text.trim().isEmpty
+          ? userData!['first_name']
+          : _firstNameController.text.trim();
+      updatedFields['last_name'] = _lastNameController.text.trim().isEmpty
+          ? userData!['last_name']
+          : _lastNameController.text.trim();
+      updatedFields['province'] = _provinceController.text.trim().isEmpty
+          ? userData!['province']
+          : _provinceController.text.trim();
+      updatedFields['city'] = _cityController.text.trim().isEmpty
+          ? userData!['city']
+          : _cityController.text.trim();
+      updatedFields['address'] = _addressController.text.trim().isEmpty
+          ? userData!['address']
+          : _addressController.text.trim();
+
+      // Send only updated fields to avoid erasing unchanged ones
+      final response = await http.patch(
+        url,
+        headers: headers,
+        body: json.encode(updatedFields),
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          userData = {...userData!, ...updatedFields};
+          isEditing = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('اطلاعات با موفقیت بروزرسانی شد')),
+        );
+      } else {
+        _showError();
+      }
+    } catch (e) {
+      _showError();
+    }
+  }
+
   void _showError() {
-    setState(() {
-      isLoading = false;
-    });
     ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('خطا در دریافت اطلاعات کاربر')),
+      const SnackBar(content: Text('خطا در برقراری ارتباط با سرور')),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return BaseWidget(
-      currentIndex: 2, // Set the active tab for the navigation bar
+      currentIndex: 2,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('پروفایل کاربری'),
         ),
         body: isLoading
             ? const Center(child: CircularProgressIndicator())
-            : userData != null
-                ? Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        _buildProfileCard(
-                            'نام', _getValue(userData!['first_name'])),
-                        _buildProfileCard(
-                            'نام خانوادگی', _getValue(userData!['last_name'])),
-                        _buildProfileCard(
-                            'شماره موبایل', _getValue(userData!['mobile'])),
-                        _buildProfileCard(
-                            'وضعیت پروفایل',
-                            userData!['profile_status'] == "shop_owner"
-                                ? 'صاحب فروشگاه'
-                                : 'کاربر عادی'),
-                        _buildProfileCard(
-                            'استان', _getValue(userData!['province'])),
-                        _buildProfileCard('شهر', _getValue(userData!['city'])),
-                        _buildProfileCard(
-                            'آدرس', _getValue(userData!['address'])),
-                      ],
-                    ),
-                  )
-                : const Center(
-                    child: Text(
-                      'اطلاعات کاربر در دسترس نیست',
-                      style: TextStyle(fontSize: 16, color: Colors.red),
+            : Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          _buildEditableField(
+                              'نام', _firstNameController, isEditing),
+                          _buildEditableField(
+                              'نام خانوادگی', _lastNameController, isEditing),
+                          _buildProfileCard(
+                              'شماره موبایل', _getValue(userData!['mobile'])),
+                          _buildProfileCard(
+                              'وضعیت پروفایل',
+                              userData!['profile_status'] == "shop_owner"
+                                  ? 'صاحب فروشگاه'
+                                  : 'کاربر عادی'),
+                          _buildEditableField(
+                              'استان', _provinceController, isEditing),
+                          _buildEditableField(
+                              'شهر', _cityController, isEditing),
+                          _buildEditableField(
+                              'آدرس', _addressController, isEditing),
+                        ],
+                      ),
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (isEditing) {
+                            _updateUserInfo();
+                          } else {
+                            setState(() {
+                              isEditing = true;
+                            });
+                          }
+                        },
+                        child: Text(
+                          isEditing ? 'ارسال' : 'ویرایش',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Widget _buildEditableField(
+      String label, TextEditingController controller, bool isEditable) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 12.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      elevation: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: isEditable
+            ? TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  labelText: label,
+                  border: const OutlineInputBorder(),
+                  contentPadding: const EdgeInsets.all(10.0),
+                ),
+                style: const TextStyle(fontSize: 16),
+                maxLines: 1,
+              )
+            : Row(
+                children: [
+                  Text(
+                    '$label:',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      controller.text.isNotEmpty ? controller.text : '---',
+                      style: const TextStyle(fontSize: 16),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
 
   Widget _buildProfileCard(String label, String value) {
     return Card(
-      margin: const EdgeInsets.only(bottom: 16.0),
+      margin: const EdgeInsets.only(bottom: 12.0),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      elevation: 3,
+      elevation: 2,
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(12.0),
         child: Row(
           children: [
             Text(
