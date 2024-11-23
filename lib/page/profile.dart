be2f736c834885fce8f721c91b4a2be06c0c01b4
Amin <asaa47721@gmail.com>
2024-11-23 07:18:22 +0330

    Implement city update, selection city with dropdown, work on styling

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:foroshgahman_application/widget/base.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:foroshgahman_application/data/province.dart';
+import '../data/city.dart'; // Ensure this file contains the cities list
+import '../data/province.dart'; // Ensure this file contains the provinces list
+import '../widget/base.dart'; // Ensure BaseWidget is properly defined
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -16,17 +17,226 @@ class _ProfilePageState extends State<ProfilePage> {
   Map<String, dynamic>? userData;
   bool isLoading = true;
   bool isEditing = false;
-  int? selectedProvinceCode; // Tracks the selected province's code
+  int? selectedProvinceCode;
+  int? selectedCityCode;
+  List<Map<String, dynamic>> filteredCities = [];
 
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
-  final TextEditingController _cityController = TextEditingController();
   final TextEditingController _addressController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _fetchUserInfo();
+  }
+
+  void _populateControllers() {
+    _firstNameController.text = userData?['first_name'] ?? '';
+    _lastNameController.text = userData?['last_name'] ?? '';
+    _addressController.text = userData?['address'] ?? '';
+  }
+
+  void _showError() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('خطا در برقراری ارتباط با سرور')),
+    );
+  }
+
+  String _getValue(dynamic value) {
+    return value != null && value.toString().isNotEmpty
+        ? value.toString()
+        : '---';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseWidget(
+      currentIndex: 2,
+      child: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        _buildEditableField(
+                            'نام', _firstNameController, isEditing),
+                        _buildEditableField(
+                            'نام خانوادگی', _lastNameController, isEditing),
+                        if (!isEditing)
+                          _buildProfileCard(
+                              'شماره موبایل', _getValue(userData!['mobile'])),
+                        if (!isEditing)
+                          _buildProfileCard(
+                              'وضعیت پروفایل',
+                              userData!['profile_status'] == "shop_owner"
+                                  ? 'صاحب فروشگاه'
+                                  : 'کاربر عادی'),
+                        _buildProvinceField(),
+                        _buildCityField(),
+                        _buildEditableField(
+                            'آدرس', _addressController, isEditing),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (isEditing) {
+                          _updateUserInfo();
+                        } else {
+                          setState(() {
+                            isEditing = true;
+                          });
+                        }
+                      },
+                      child: Text(
+                        isEditing ? 'ارسال' : 'ویرایش',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+
+  Widget _buildEditableField(
+      String label, TextEditingController controller, bool isEditable) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: isEditable
+            ? TextField(
+                controller: controller,
+                decoration: InputDecoration(labelText: label),
+              )
+            : Row(
+                children: [
+                  Text('$label:'),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                        controller.text.isNotEmpty ? controller.text : '---'),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Widget _buildProfileCard(String label, String value) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            Text('$label:'),
+            const SizedBox(width: 8),
+            Expanded(child: Text(value)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProvinceField() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: isEditing
+            ? DropdownButtonFormField<int>(
+                value: selectedProvinceCode,
+                decoration: const InputDecoration(labelText: 'استان'),
+                onChanged: (int? newValue) {
+                  setState(() {
+                    selectedProvinceCode = newValue;
+                    _filterCities(selectedProvinceCode);
+                    selectedCityCode = null;
+                  });
+                },
+                items: provinces.map((province) {
+                  return DropdownMenuItem<int>(
+                    value: province['code'],
+                    child: Text(province['name']),
+                  );
+                }).toList(),
+              )
+            : Row(
+                children: [
+                  const Text('استان:'),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      provinces
+                          .firstWhere(
+                            (province) =>
+                                province['code'] == selectedProvinceCode,
+                            orElse: () => {'name': 'نامشخص'},
+                          )['name']
+                          .toString(),
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Widget _buildCityField() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: isEditing
+            ? DropdownButtonFormField<int>(
+                value: selectedCityCode,
+                decoration: const InputDecoration(labelText: 'شهر'),
+                onChanged: (int? newValue) {
+                  setState(() {
+                    selectedCityCode = newValue;
+                  });
+                },
+                items: filteredCities.map((city) {
+                  return DropdownMenuItem<int>(
+                    value: city['city_code'],
+                    child: Text(city['name']),
+                  );
+                }).toList(),
+              )
+            : Row(
+                children: [
+                  const Text('شهر:'),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      filteredCities
+                          .firstWhere(
+                            (city) => city['city_code'] == selectedCityCode,
+                            orElse: () => {'name': 'نامشخص'},
+                          )['name']
+                          .toString(),
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
   }
 
   Future<void> _fetchUserInfo() async {
@@ -46,9 +256,15 @@ class _ProfilePageState extends State<ProfilePage> {
         final data = json.decode(utf8.decode(response.bodyBytes));
         setState(() {
           userData = data;
+
+          // Set the selected province and city codes
           selectedProvinceCode = data['province_code'];
+          selectedCityCode = data['city_code'];
+
+          // Populate the controllers with user data
           _populateControllers();
           isLoading = false;
+          _filterCities(selectedProvinceCode);
         });
       } else {
         _showError();
@@ -56,13 +272,6 @@ class _ProfilePageState extends State<ProfilePage> {
     } catch (e) {
       _showError();
     }
-  }
-
-  void _populateControllers() {
-    _firstNameController.text = userData?['first_name'] ?? '';
-    _lastNameController.text = userData?['last_name'] ?? '';
-    _cityController.text = userData?['city'] ?? '';
-    _addressController.text = userData?['address'] ?? '';
   }
 
   Future<void> _updateUserInfo() async {
@@ -76,7 +285,6 @@ class _ProfilePageState extends State<ProfilePage> {
         'Content-Type': 'application/json',
       };
 
-      // Prepare the fields for updating, keeping unchanged fields the same as before
       final Map<String, dynamic> updatedFields = {};
 
       updatedFields['first_name'] = _firstNameController.text.trim().isEmpty
@@ -85,16 +293,18 @@ class _ProfilePageState extends State<ProfilePage> {
       updatedFields['last_name'] = _lastNameController.text.trim().isEmpty
           ? userData!['last_name']
           : _lastNameController.text.trim();
+
+      // Send province_code directly
       updatedFields['province_code'] =
           selectedProvinceCode ?? userData!['province_code'];
-      updatedFields['city'] = _cityController.text.trim().isEmpty
-          ? userData!['city']
-          : _cityController.text.trim();
+
+      // Send city_code directly from selectedCityCode
+      updatedFields['city_code'] = selectedCityCode ?? userData!['city_code'];
+
       updatedFields['address'] = _addressController.text.trim().isEmpty
           ? userData!['address']
           : _addressController.text.trim();
 
-      // Send only updated fields to avoid erasing unchanged ones
       final response = await http.patch(
         url,
         headers: headers,
@@ -117,222 +327,16 @@ class _ProfilePageState extends State<ProfilePage> {
     }
   }
 
-  void _showError() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('خطا در برقراری ارتباط با سرور')),
-    );
-  }
+  void _filterCities(int? provinceCode) {
+    setState(() {
+      filteredCities = cities
+          .where((city) => city['province_code'] == provinceCode)
+          .toList();
 
-  @override
-  Widget build(BuildContext context) {
-    return BaseWidget(
-      currentIndex: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('پروفایل کاربری'),
-        ),
-        body: isLoading
-            ? const Center(child: CircularProgressIndicator())
-            : Column(
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          _buildEditableField(
-                              'نام', _firstNameController, isEditing),
-                          _buildEditableField(
-                              'نام خانوادگی', _lastNameController, isEditing),
-                          _buildProfileCard(
-                              'شماره موبایل', _getValue(userData!['mobile'])),
-                          _buildProfileCard(
-                              'وضعیت پروفایل',
-                              userData!['profile_status'] == "shop_owner"
-                                  ? 'صاحب فروشگاه'
-                                  : 'کاربر عادی'),
-                          _buildProvinceDropdown(),
-                          _buildEditableField(
-                              'شهر', _cityController, isEditing),
-                          _buildEditableField(
-                              'آدرس', _addressController, isEditing),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blueAccent,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                        onPressed: () {
-                          if (isEditing) {
-                            _updateUserInfo();
-                          } else {
-                            setState(() {
-                              isEditing = true;
-                            });
-                          }
-                        },
-                        child: Text(
-                          isEditing ? 'ارسال' : 'ویرایش',
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-      ),
-    );
-  }
-
-  Widget _buildEditableField(
-      String label, TextEditingController controller, bool isEditable) {
-    return Card(
-      margin: const EdgeInsets.only(bottom: 12.0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: isEditable
-            ? TextField(
-                controller: controller,
-                decoration: InputDecoration(
-                  labelText: label,
-                  border: const OutlineInputBorder(),
-                  contentPadding: const EdgeInsets.all(10.0),
-                ),
-                style: const TextStyle(fontSize: 16),
-                maxLines: 1,
-              )
-            : Row(
-                children: [
-                  Text(
-                    '$label:',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blueGrey,
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      controller.text.isNotEmpty ? controller.text : '---',
-                      style: const TextStyle(fontSize: 16),
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                ],
-              ),
-      ),
-    );
-  }
-
-  Widget _buildProfileCard(String label, String value) {
-    return Card(
-      margin: const EdgeInsets.only(bottom: 12.0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Row(
-          children: [
-            Text(
-              '$label:',
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.blueGrey,
-              ),
-            ),
-            const SizedBox(width: 8),
-            Expanded(
-              child: Text(
-                value,
-                style: const TextStyle(fontSize: 16),
-                textAlign: TextAlign.end,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildProvinceDropdown() {
-    return Card(
-      margin: const EdgeInsets.only(bottom: 8.0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: isEditing
-            ? DropdownButton<int>(
-                value: selectedProvinceCode,
-                isExpanded: true,
-                hint: const Text('انتخاب استان'),
-                items: provinces.map((province) {
-                  return DropdownMenuItem<int>(
-                    value: province['code'],
-                    child: Text(province['name']),
-                  );
-                }).toList(),
-                onChanged: (int? value) {
-                  setState(() {
-                    selectedProvinceCode = value;
-                  });
-                },
-              )
-            : Row(
-                children: [
-                  const Text(
-                    'استان:',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blueGrey,
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      _getProvinceName(selectedProvinceCode) ?? '---',
-                      style: const TextStyle(fontSize: 14),
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                ],
-              ),
-      ),
-    );
-  }
-
-  String? _getProvinceName(int? code) {
-    if (code == null) return null;
-
-    final province = provinces.firstWhere(
-      (p) => p['code'] == code,
-      orElse: () => {'name': null}, // Return a dummy Map
-    );
-
-    return province['name'];
-  }
-
-  String _getValue(String? value) {
-    return (value == null || value.trim().isEmpty) ? '---' : value;
+      if (!filteredCities
+          .any((city) => city['city_code'] == selectedCityCode)) {
+        selectedCityCode = null;
+      }
+    });
   }
 }
